b53f04380262c4decdde5b64ff4288d93d16c34a
Anselme Donato <anselme.donato2@gmail.com>
2023-04-25 09:12:36 +0200

    Hello world component

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  template: `
+    <p>
+      hello-world works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HelloWorldComponent {
+
+}

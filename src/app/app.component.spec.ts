0fbb9e006260604df9d880df8fedbcb6548e1bc7
Anselme Donato <anselme.donato2@gmail.com>
2023-04-28 13:21:34 +0200

    Adding a test with ATL

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { render, screen } from '@testing-library/angular';
 
 describe('AppComponent', () => {
 	beforeEach(async () => {
@@ -20,16 +21,25 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have as title 'AgenDaz'`, () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		const app = fixture.componentInstance;
-		expect(app.title).toEqual('AgenDaz');
-	});
+	// it('should have as title [AgenDaz]', () => {
+	// 	const fixture = TestBed.createComponent(AppComponent);
+	// 	const app = fixture.componentInstance;
+	// 	expect(app.title).toEqual('AgenDaz');
+	// });
 
-	it('should render title', () => {
-		const fixture = TestBed.createComponent(AppComponent);
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector('.content span')?.textContent).toContain('AgenDaz app is running!');
-	});
+  // This one is a legacy test, it should not pass now
+	// it('should render title', () => {
+	// 	const fixture = TestBed.createComponent(AppComponent);
+	// 	fixture.detectChanges();
+	// 	const compiled = fixture.nativeElement as HTMLElement;
+	// 	expect(compiled.querySelector('.content span')?.textContent).toContain('AgenDaz app is running!');
+	// });
+
+  // describe('With testing library', () => {
+  //   it('should have AgenDaz header', async () => {
+  //     await render(AppComponent);
+  //     const header = screen.getByRole('heading', { name: 'AgenDaz'});
+  //     expect(header).toBeTruthy();
+  //   })
+  // })
 });
